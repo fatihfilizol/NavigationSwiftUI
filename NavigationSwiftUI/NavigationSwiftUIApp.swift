e9abb3e6862eb6f2efd48ed1a0f804749a65371f
fatihfilizol <fatihfilizol@gmail.com>
2022-11-13 15:34:38 +0300

    Initial Commit

---
+++ NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by Fatih Filizol on 13.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
